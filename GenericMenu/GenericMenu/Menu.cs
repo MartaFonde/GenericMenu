3450c79539170c4f36863bc6bd09cb4e57c966ec
martafonde <marta4510@hotmail.com>
2020-11-07 11:31:44 +0100

    v1

---
+++ GenericMenu/GenericMenu/Menu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMenu
+{
+    class Menu
+    {
+        public static void MenuGenerator(string[] options, MyDelegate[] functions)
+        {
+            if (options != null && functions != null)
+            {
+                int op;
+                do
+                {
+                    for (int i = 1; i <= options.Length + 1; i++)
+                    {
+                        if (i <= options.Length)
+                        {
+                            Console.WriteLine(i + ". " + options[i - 1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine(i + ". Exit");
+                        }
+                    }
+                    op = ValidarNum(options.Length + 1, 1);
+                    if (op <= options.Length)
+                    {
+                        MyDelegate f = functions[op - 1];
+                        f();
+                        Console.WriteLine();
+                    }
+                } while (op < options.Length + 1);
+            }
+        }
+
+        public static int ValidarNum(int max, int min = 0)
+        {
+            bool valido = false;
+            int num = 0;
+            while (!valido)
+            {
+                try
+                {
+                    valido = true;
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (num < min || num > max)
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("The number is out of range " + min + "-" + max);
+                    valido = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The data entered must be a number");
+                    valido = false;
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ GenericMenu/GenericMenu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericMenu
+{
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void f1()
+        {
+            Console.WriteLine("A");
+        }
+        static void f2()
+        {
+            Console.WriteLine("B");
+        }
+        static void f3()
+        {
+            Console.WriteLine("C");
+        }
+
+        static void Main(string[] args)
+        {
+            Menu.MenuGenerator(new string[] { "Op1", "Op2", "Op3" }, new MyDelegate[] { f1, f2, f3 });
+            Console.ReadKey();
+        }
+
+    }
+}
